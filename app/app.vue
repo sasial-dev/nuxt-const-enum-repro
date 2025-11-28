2e20fcb8293cd9ae4698047e806774aa3b0021fe
Sasial <44125644+sasial-dev@users.noreply.github.com>
2025-11-28 14:14:51 +1000

    Initial Commit 🎉

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+	<div></div>
+</template>
+
+<script setup lang="ts">
+// import { Foo } from '#imports';
+
+function test(a: string, b: Foo.Bar): void;
+function test(a: number, b: Foo.Baz): void;
+function test(a: string | number, b: Foo): void {
+	console.log(Foo.Bar);
+}
+</script>
